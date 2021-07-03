8e674c27809c2500a3975e5e4cd4304b1e70bfd9
jeongbinboo <jrkasey3461@naver.com>
2021-07-03 13:18:17 +0900

    commit

--- DFS+BFS/2065_캔디팡.py
+++ DFS+BFS/2065_캔디팡.py
@@ -1,11 +1,19 @@
-def pang(i,j,c):
-    print(i,j)
-    if i<0 or i>=7 or j<0 or j>=7:
+def pang(x,y,cmp):
+    global cnt
+    if x<0 or x>=7 or y<0 or y>=7:
         return 0
-    if candy[i][j]==c:
-        candy[i][j]=0
-    return 1+pang(i-1,j,c)+pang(i+1,j,c)+pang(i,j-1,c)+pang(i,j+1,c)
+    if candy[x][y]==cmp:
+        candy[x][y]=0
+        cnt+=1
+        pang(x-1,y,cmp)
+        pang(x+1,y,cmp)
+        pang(x,y-1,cmp)
+        pang(x,y+1,cmp)
+        return cnt
+    return 0
+
 candy=[]
+cnt=0
 res=0
 for i in range(7):
     candy.append(list(map(int,input().split())))
@@ -13,8 +21,7 @@ for i in range(7):
 for i in range(7):
     for j in range(7):
         cnt=0
-        if pang(i,j,candy[i][j])>=3:
-            res+=1
-        
+        if candy[i][j]!=0:
+            if pang(i,j,candy[i][j])>=3:
+                res+=1
 print(res)
-print(candy)
--- DFS+BFS/4503_바이러스.py
+++ DFS+BFS/4503_바이러스.py
@@ -1,14 +1,19 @@
+def find(cp):
+    checked.append(cp)
+    for i in range(n+1):
+        if com[cp][i]==1 and i not in checked:
+            find(i)
+
 n=int(input())
 m=int(input())
-cp=[]
-chk=[]
-for i in range(n):
-    cp.append([0,0,0,0,0,0,0])
-    chk.append(0)
-for i in range(m):
-    a,b=map(int,input().split())
-    cp[a-1][b-1]=1
-    cp[b-1][a-1]=1
+checked=[]
 
-print(cp)
-print(chk)
+com=[[0]*(n+1) for i in range(n+1)]
+
+for i in range(m):
+    c1,c2=map(int,input().split())
+    com[c1][c2]=com[c2][c1]=1
+    
+
+find(1)
+print(len(checked)-1)
